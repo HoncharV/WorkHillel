d13bd95f5cf10a68db08d3325d40ddd280cb6e06
user <user@WIN-H2QPH3A41C1>
2024-04-08 14:15:24 +0300

    HomeWork2

--- Home_Work_2/Program.cs
+++ Home_Work_2/Program.cs
@@ -1,17 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-//а) Обчислення величини знижки 
-int price = 100;
-byte discount = 12;
-int netPrice = discount * price / 100;
-Console.WriteLine($"Ціна з знижкою: {netPrice}");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //а) Обчислення величини знижки 
+        int price = 100;
+        int discount = 12;
+        int netPrice = discount * price / 100;
 
-
-//б) Час в секундах
-byte hour = 9;
-byte minutes = 27;
-int seconds = 15;
-//в) Створіть програму, яка виводить інформацію про книгу
-string nameBook = ("Atlantis");
-string writer = ("Ihor Kutsenko");
-int year = 1950;
-int priceBook = 1500;
+        //б) Час в секундах
+        byte hour = 9;
+        byte minutes = 27;
+        int seconds = 15;
+        //в) Створіть програму, яка виводить інформацію про книгу
+        string nameBook = "Atlantis";
+        string writer = "Ihor Kutsenko";
+        int year = 1950;
+        int priceBook = 1500;
+       
+    }
+}
