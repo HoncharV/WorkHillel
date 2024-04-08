c6bdd15ef11c70d4621885087d95c26ef0144f25
user <user@WIN-H2QPH3A41C1>
2024-04-08 15:51:24 +0300

    change 1

--- Home_Work_2/Program.cs
+++ Home_Work_2/Program.cs
@@ -2,11 +2,14 @@
 {
     private static void Main(string[] args)
     {
+
         //а) Обчислення величини знижки 
+        Console.Output.Encoding = System.Text.Encoding.Unicode;
         int price = 100;
         int discount = 12;
         int netPrice = (price - (discount * price / 100));
-        Console.WriteLine($"ціна із знижкою " + { netPrice});
+        Console.WriteLine("ціна із знижкою");
+        Console.WriteLine(netPrice);
 
         //б) Час в секундах
         byte hour = 9;
@@ -17,6 +20,7 @@
         string writer = "Ihor Kutsenko";
         int year = 1950;
         int priceBook = 1500;
+        
         Console.ReadKey();
 
     }
