12f577a27e840f040673ae8c254cf4dd7ed64212
user <user@WIN-H2QPH3A41C1>
2024-04-08 13:55:31 +0300

    HomeWork 2

---
+++ Home_Work_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//а) Обчислення величини знижки 
+int price = 100;
+byte discount = 12;
+int netPrice = discount * price / 100;
+Console.WriteLine($"Ціна з знижкою: {netPrice}");
+
+
+//б) Час в секундах
+byte hour = 9;
+byte minutes = 27;
+int seconds = 15;
+//в) Створіть програму, яка виводить інформацію про книгу
+string nameBook = ("Atlantis");
+string writer = ("Ihor Kutsenko");
+int year = 1950;
+int priceBook = 1500;
